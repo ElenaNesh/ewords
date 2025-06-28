ac5a539f2f3d3f42f452850a70bb066415af6aef
ElenaNesh <neshataeva.elena@gmail.com>
2025-06-28 12:50:55 +0300

    импорт навбара исправила

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import words from './components/Words';
 import Header from './components/Header';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import TopicPage from './components/TopicPage';
 import WordsList from './components/WordsList';
